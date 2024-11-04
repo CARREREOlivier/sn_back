c5ab88829970ec00d6a7138fec80e194bec99065
CARREREOlivier <carrereolivier@yahoo.fr>
2024-11-04 09:57:59 +0100

    Fix: Routings

--- public/index.php
+++ public/index.php
@@ -2,5 +2,20 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../config/database.php';
-require_once __DIR__ . '/../routes/auth_routes.php';
+
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+switch ($requestUri) {
+    case '/recit':
+        require_once __DIR__ . '/../routes/recit_routes.php';
+        break;
+    case '/login':
+    case '/logout':
+        require_once __DIR__ . '/../routes/auth_routes.php';
+        break;
+    default:
+        echo json_encode(['message' => 'Route not found']);
+        http_response_code(404);
+        break;
+}
 ?>
