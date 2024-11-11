30f14105c8db49c2cbc05562c01b34d05dd06938
CARREREOlivier <carrereolivier@yahoo.fr>
2024-11-11 15:02:12 +0100

    fix: index file to manage properly routes.

--- public/index.php
+++ public/index.php
@@ -1,6 +1,17 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../routes/recit_routes.php';
+$requestUri = $_SERVER['REQUEST_URI'];
 
-?>
+// Redirige vers user_routes.php pour les chemins /users
+if (preg_match('#^/users(/.*)?$#', $requestUri)) {
+    require_once __DIR__ . '/../routes/user_routes.php';
+}
+// Redirige vers article_routes.php pour les chemins /recits
+elseif (preg_match('#^/recits/([a-zA-Z0-9_-]+)/(new-article|([a-zA-Z0-9_-]+(/(update-article|delete-article)?)?))$#', $requestUri)) {
+    require_once __DIR__ . '/../routes/article_routes.php';
+}
+// Par défaut, redirige vers recit_routes.php
+else {
+    require_once __DIR__ . '/../routes/recit_routes.php';
+}
